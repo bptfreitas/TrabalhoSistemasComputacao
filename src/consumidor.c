60a96d52ebcb3cc8cbeacb45a5fc29123e8b1a13
Bruno <bruno.freitas@cefet-rj.br>
2024-11-20 14:20:56 -0300

    Added initial S_t struct processing

--- src/consumidor.c
+++ src/consumidor.c
@@ -9,7 +9,50 @@ void* consumidor(void* args){
 
     buffer_t *buffer_do_cp3 = &buffers[3];
 
-    fprintf(stdout, "\nStarting consumer thread ... " );    
+    fprintf(stdout, "\nStarting consumer thread ... " );
+
+    while (1){
+
+        sem_wait( &buffer_do_cp3->empty );
+
+        S_t *data;
+
+        int out;
+
+        sem_wait( &buffer_do_cp3->mutex );    
+
+        out = buffer_do_cp3->out;
+
+        data = buffer_do_cp3->data[ out % BUFFER_SIZE ];
+
+        buffer_do_cp3->data[ out % BUFFER_SIZE ] = NULL;
+
+        buffer_do_cp3->out ++;
+
+        sem_post( &buffer_do_cp3->mutex );
+
+        char output_filename[256];
+
+        strcpy( output_filename, data->source_filename);
+
+        strcat( data->source_filename, "saida.out" );
+
+        FILE *output_fd = fopen( output_filename, "w");
+
+        if (output_fd == NULL){
+
+            perror("Error:");
+
+            continue;
+
+        }
+        
+
+        // Close file after use
+
+        fclose( output_fd );
+
+    }
 
    
 }
