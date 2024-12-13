3963dca1d187678d02e81a0d05226aabfbe8222c
Bruno <bruno.freitas@cefet-rj.br>
2024-12-13 18:20:01 -0300

    Added script to test thread controller

---
+++ scripts/test_thread_controller.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "-d cp3" > /tmp/matrix_deamon  ; 
+sleep 1 ; 
+echo "-c cp1" > /tmp/matrix_deamon ; 
+sleep 1 ; 
+echo "-c cp1" > /tmp/matrix_deamon ; 
+sleep 1; 
+echo "-d cp1" > /tmp/matrix_deamon ;
